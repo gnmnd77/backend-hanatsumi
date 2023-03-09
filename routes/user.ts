b56be6181d4d7c45a5075dc081fe44552cf3ece3
noda <noda@noda.local>
2023-03-09 18:20:42 +0900

    フォローとフォロー解除のAPIを追加

--- routes/user.ts
+++ routes/user.ts
@@ -8,8 +8,8 @@ type UserType = {
     password: string;
     profileImage: string;
     coverImage: string;
-    followers: any[];
-    followings: any[];
+    followers: string[];
+    followings: string[];
     description?: string | undefined;
     city?: string | undefined;
     updatedAt: string;
@@ -22,7 +22,7 @@ const router = express.Router();
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user: UserType = await User.findByIdAndUpdate(req.params.id, {
+      const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
       if (!user) return res.status(404).json("ユーザーが存在しません");
@@ -39,7 +39,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user: UserType = await User.findByIdAndDelete(req.params.id);
+      const user = await User.findByIdAndDelete(req.params.id);
 
       return res.status(200).json("ユーザー情報が削除されました");
     } catch (err) {
@@ -59,6 +59,70 @@ router.get("/:id", async (req, res) => {
     return res.status(200).json(other);
   } catch (err) {
     return res.status(500).json(err);
+  }
+});
+
+//ユーザーをフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.params.id !== req.body.userId) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user?.followers.includes(currentUser?.id)) {
+        await user?.updateOne({
+          $push: {
+            followers: currentUser?.id,
+          },
+        });
+
+        await currentUser?.updateOne({
+          $push: {
+            followings: user?.id,
+          },
+        });
+
+        return res.status(200).json("ユーザーをフォローしました");
+      } else {
+        return res.status(403).json("既にこのユーザーをフォローしています");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("あなた自身をフォローする事はできません");
+  }
+});
+
+//フォロー解除
+router.put("/:id/remove", async (req, res) => {
+  if (req.params.id !== req.body.userId) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (user?.followers.includes(currentUser?.id)) {
+        await user?.updateOne({
+          $pull: {
+            followers: currentUser?.id,
+          },
+        });
+
+        await currentUser?.updateOne({
+          $pull: {
+            followings: user?.id,
+          },
+        });
+
+        return res.status(200).json("フォローを解除しました");
+      } else {
+        return res.status(403).json("このユーザーをフォローしていません");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("あなた自身をフォロー解除する事はできません");
   }
 });
 
