f60025006db7de8a149105ea4051be8c7baba097
noda <noda@noda.local>
2023-03-10 10:59:13 +0900

    投稿単体取得といいね機能のAPIを追加

--- routes/post.ts
+++ routes/post.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import Post from "../models/Post";
+import User from "../models/User";
 
 const router = express.Router();
 
@@ -47,6 +48,45 @@ router.delete("/:id", async (req, res) => {
     }
   } catch (err) {
     return res.status(403).json(err);
+  }
+});
+
+//投稿単体取得
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(403).json(err);
+  }
+});
+
+//いいね機能
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const currentUser = await User.findById(req.body.userId);
+
+    if (!post?.likes.includes(currentUser?.id)) {
+      await post?.updateOne({
+        $push: {
+          likes: currentUser?.id,
+        },
+      });
+
+      return res.status(200).json("投稿にいいねをしました");
+    } else {
+      await post?.updateOne({
+        $pull: {
+          likes: currentUser?.id,
+        },
+      });
+
+      return res.status(200).json("いいねを取り消しました");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
   }
 });
 
